24ebf62c13dafea6f02dbfea1ff0465adf9be0f2
Chris Troutner <chris.troutner@gmail.com>
2022-09-07 15:06:11 -0700

    fix(adapters): Added log and bch adapters and unit tests

--- index.js
+++ index.js
@@ -5,9 +5,12 @@
   See the specification document in the dev-docs directory.
 */
 
+// Local libraries
+import Adapters from './lib/adapters/index.js'
+
 class IpfsCoord {
   constructor (localConfig = {}) {
-    // Input Validation
+    // Dependency Injection
     if (!localConfig.ipfs) {
       throw new Error(
         'An instance of IPFS must be passed when instantiating the ipfs-coord library.'
@@ -24,6 +27,40 @@ class IpfsCoord {
         'The type of IPFS node (browser or node.js) must be specified.'
       )
     }
+
+
+    // Retrieve and/or set the debug level.
+    // 0 = no debug information.
+    // 1 = status logs
+    // 2 = verbose errors about peer connections
+    this.debugLevel = parseInt(localConfig.debugLevel)
+    if (!this.debugLevel) this.debugLevel = 0
+    localConfig.debugLevel = this.debugLevel
+    console.log(`ipfs-coord debug level: ${localConfig.debugLevel}`)
+
+    // localConfiguration of an optional 'status' log handler for log reports. If none
+    // is specified, defaults to console.log.
+    if (localConfig.statusLog) {
+      this.statusLog = localConfig.statusLog
+    } else {
+      this.statusLog = console.log
+    }
+    // If the statusLog handler wasn't specified, then define it.
+    localConfig.statusLog = this.statusLog
+
+    // localConfiguration of an optional 'private' log handler for recieving e2e
+    // encrypted message. If none is specified, default to console.log.
+    if (localConfig.privateLog) {
+      this.privateLog = localConfig.privateLog
+    } else {
+      this.privateLog = console.log
+    }
+    // If the privateLog handler wasn't specified, then define it.
+    localConfig.privateLog = this.privateLog
+
+    // Load the adapter libraries.
+    this.adapters = new Adapters(localConfig)
+    localConfig.adapters = this.adapters
   }
 }
 
--- test/unit/index-unit.js
+++ test/unit/index-unit.js
@@ -3,23 +3,26 @@
 */
 
 // Global npm libraries
-import {assert} from 'chai'
+import { assert } from 'chai'
 import sinon from 'sinon'
-import SlpWallet from 'minimal-slp-wallet'
+import SlpWallet from 'minimal-slp-wallet/index.js'
 
 // Local libraries
 import IpfsCoord from '../../index.js'
 import ipfs from '../mocks/ipfs-mock.js'
 
-describe('#ipfs-coord', () => {
+describe('#ipfs-coord - index.js', () => {
   let sandbox
   let uut // Unit Under Test
+  let wallet
 
-  beforeEach(() => {
+  beforeEach(async () => {
     // Restore the sandbox before each test.
     sandbox = sinon.createSandbox()
 
-    const wallet = new SlpWallet()
+    wallet = new SlpWallet()
+    await wallet.walletInfoPromise
+
     uut = new IpfsCoord({ wallet, ipfs, type: 'node.js' })
   })
 
@@ -64,6 +67,59 @@ describe('#ipfs-coord', () => {
           'The type of IPFS node (browser or node.js) must be specified.'
         )
       }
+    })
+
+    it('should override default logs', async () => {
+
+
+      uut = new IpfsCoord({
+        wallet,
+        ipfs,
+        type: 'node.js',
+        statusLog: console.log,
+        privateLog: console.log
+      })
+    })
+
+    it('should set debugLevel to 0 if not specified', () => {
+
+      uut = new IpfsCoord({
+        wallet,
+        ipfs,
+        type: 'node.js',
+        statusLog: console.log,
+        privateLog: console.log
+      })
+
+      assert.equal(uut.debugLevel, 0)
+    })
+
+    it('should set debugLevel to 2 if specified', () => {
+
+      uut = new IpfsCoord({
+        wallet,
+        ipfs,
+        type: 'node.js',
+        statusLog: console.log,
+        privateLog: console.log,
+        debugLevel: 2
+      })
+
+      assert.equal(uut.debugLevel, 2)
+    })
+
+    it('should default debugLevel to 0 non-integer is used', () => {
+
+      uut = new IpfsCoord({
+        wallet,
+        ipfs,
+        type: 'node.js',
+        statusLog: console.log,
+        privateLog: console.log,
+        debugLevel: 'abcd'
+      })
+
+      assert.equal(uut.debugLevel, 0)
     })
   })
 })
