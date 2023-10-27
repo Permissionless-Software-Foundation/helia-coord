eb5c28236c00db9452929d87b6d9b7ba49d71b32
Chris Troutner <chris.troutner@gmail.com>
2023-10-27 10:26:54 -0700

    Improved handling of pubsub message

---
+++ config/global-config.js
@@ -0,0 +1,10 @@
+/*
+  Config file for storing global configuration settings.
+*/
+
+const config = {
+  DEFAULT_COORDINATION_ROOM: 'psf-ipfs-coordination-002',
+  BCH_COINJOIN_ROOM: 'bch-coinjoin-001'
+}
+
+export default config
