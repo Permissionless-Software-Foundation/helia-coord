6da8f49137fc9e5941311c8cc22399a62a4bc2f8
Chris Troutner <chris.troutner@gmail.com>
2023-12-02 14:01:15 -0800

    fix(connectToBoostrapRelays()): Repurposed old connctToV1CircuitRelays()

--- config/bootstrap-circuit-relays.js
+++ config/bootstrap-circuit-relays.js
@@ -5,36 +5,29 @@
 /* eslint camelcase: 0 */
 
 const BOOTSTRAP_BROWSER_CRs = [
-  {
-    // Based in USA, east coast.
-    name: 'bchd.nl',
-    multiaddr: '/dns4/bchd.nl/tcp/443/wss/p2p/12D3KooWRBhwfeP2Y9CDkFRBAZ1pmxUadH36TKuk3KtKm5XXP8mA',
-    connected: false,
-    ipfsId: '12D3KooWRBhwfeP2Y9CDkFRBAZ1pmxUadH36TKuk3KtKm5XXP8mA',
-    isBootstrap: true
-  },
-  {
-    name: 'ipfs-cr.fullstackslp.nl',
-    multiaddr: '/dns4/ipfs-cr.fullstackslp.nl/tcp/443/wss/p2p/12D3KooWJyc54njjeZGbLew4D8u1ghrmZTTPyh3QpBF7dxtd3zGY',
-    connected: false,
-    ipfsId: '12D3KooWJyc54njjeZGbLew4D8u1ghrmZTTPyh3QpBF7dxtd3zGY',
-    isBootstrap: true
-  }
+  // {
+  //   // Based in USA, east coast.
+  //   name: 'bchd.nl',
+  //   multiaddr: '/dns4/bchd.nl/tcp/443/wss/p2p/12D3KooWRBhwfeP2Y9CDkFRBAZ1pmxUadH36TKuk3KtKm5XXP8mA',
+  //   connected: false,
+  //   ipfsId: '12D3KooWRBhwfeP2Y9CDkFRBAZ1pmxUadH36TKuk3KtKm5XXP8mA',
+  //   isBootstrap: true
+  // },
+  // {
+  //   name: 'ipfs-cr.fullstackslp.nl',
+  //   multiaddr: '/dns4/ipfs-cr.fullstackslp.nl/tcp/443/wss/p2p/12D3KooWJyc54njjeZGbLew4D8u1ghrmZTTPyh3QpBF7dxtd3zGY',
+  //   connected: false,
+  //   ipfsId: '12D3KooWJyc54njjeZGbLew4D8u1ghrmZTTPyh3QpBF7dxtd3zGY',
+  //   isBootstrap: true
+  // }
 ]
 
 const BOOTSTRAP_NODE_CRs = [
   {
-    name: 'bchd.nl',
-    multiaddr: '/ip4/5.161.95.233/tcp/4001/p2p/12D3KooWRBhwfeP2Y9CDkFRBAZ1pmxUadH36TKuk3KtKm5XXP8mA',
+    name: 'psf1.tokentiger.com',
+    multiaddr: '/ip4/137.184.93.145/tcp/4001/p2p/12D3KooWGZCpD5Ue3CJCBBEKowcuKEgeVKbTM7VMbJ8xm1bqST1j',
     connected: false,
-    ipfsId: '12D3KooWRBhwfeP2Y9CDkFRBAZ1pmxUadH36TKuk3KtKm5XXP8mA',
-    isBootstrap: true
-  },
-  {
-    name: 'ipfs-cr.fullstackslp.nl',
-    multiaddr: '/ip4/78.46.129.7/tcp/4001/p2p/12D3KooWJyc54njjeZGbLew4D8u1ghrmZTTPyh3QpBF7dxtd3zGY',
-    connected: false,
-    ipfsId: '12D3KooWJyc54njjeZGbLew4D8u1ghrmZTTPyh3QpBF7dxtd3zGY',
+    ipfsId: '12D3KooWGZCpD5Ue3CJCBBEKowcuKEgeVKbTM7VMbJ8xm1bqST1j',
     isBootstrap: true
   }
 ]
