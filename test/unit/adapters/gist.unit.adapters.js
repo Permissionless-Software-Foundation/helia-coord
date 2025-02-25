904218cb6427094a38390f40db83540a26b66146
Chris Troutner <chris.troutner@gmail.com>
2025-02-25 13:04:29 -0700

    fix(gist adapter): Unit test coverage back to 100% coverage

--- test/unit/adapters/gist.unit.adapters.js
+++ test/unit/adapters/gist.unit.adapters.js
@@ -55,4 +55,37 @@ describe('#Adapter-Gist', () => {
       }
     })
   })
+
+  describe('#getCRList2', () => {
+    it('should get data from GitHub', async () => {
+      // Mock network dependencies
+      sandbox.stub(uut.axios, 'get').resolves({
+        data: {
+          files: {
+            'psf-helia-public-circuit-relays.json': {
+              content: JSON.stringify({ key: 'value' })
+            }
+          }
+        }
+      })
+
+      const result = await uut.getCRList2()
+      // console.log('result: ', result)
+
+      assert.property(result, 'files')
+    })
+
+    it('should catch and throw errors', async () => {
+      try {
+        // Force desired code path
+        sandbox.stub(uut.axios, 'get').rejects(new Error('test error'))
+
+        await uut.getCRList2()
+
+        assert.fail('Unexpected result')
+      } catch (err) {
+        assert.include(err.message, 'test error')
+      }
+    })
+  })
 })
